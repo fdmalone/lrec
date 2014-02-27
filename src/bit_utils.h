97d90b9cd3655b027fdf0534564ea1e7583874c4
Fionn Malone <f.malone13@imperial.ac.uk>
2014-02-27 15:00:24 +0000

    Completed purge.

--- src/bit_utils.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <stdint.h>
-// Bit Manipulation.
-uint16_t rotl(uint16_t x, int n);
-uint16_t rotr(uint16_t x, int n);
-// Bit twiddling hacks
-uint16_t swap_bits(uint16_t x);
-int count_bits(uint16_t x);
--- src/sorting.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <vector>
-// Sorting;
-int permute(int a, int b, double &sign);
-int permute_norm(int a, int b, double &sign);
-int insertion_sort(int array[], int lenght);
-int insertion_rank(uint16_t array[], uint16_t rank[], int length);
-int binary_search(vector<uint16_t> &a, int min, int max, uint16_t val, int &pos);
-int look_up_table(uint16_t input, uint16_t arr[]);
